9575f349c26192905442da4daa806f2ddf2c0855
sreedhar_bellam <sreedhar.b@gmail.com>
2019-12-04 12:39:31 +0530

    header

--- src/pages/staticpage/staticpage.ts
+++ src/pages/staticpage/staticpage.ts
@@ -9,8 +9,6 @@ export class StaticpagePage {
 title:String="static";
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad StaticpagePage');
-  }
-
+ menus=["Menu1","Menu2","Menu3","Menu4","Menu5","Menu6","Menu7","Menu8","Menu9"];
+ links=["Link1","Link2","Link3","Link4","Link5","Link6","Link7","Link8","Link9"];
 }
