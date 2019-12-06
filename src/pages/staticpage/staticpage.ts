446a7dd9f975895bdd33655b2916fd93a3658fe4
sreedhar_bellam <sreedhar.b@gmail.com>
2019-12-06 11:10:02 +0530

    dont Know

--- src/pages/staticpage/staticpage.ts
+++ src/pages/staticpage/staticpage.ts
@@ -7,8 +7,20 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class StaticpagePage {
 title:String="static";
+menuLinks: Array<object>;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.menuLinks=[
+      {menu:'menu1', link:'link1'},
+      {menu:'menu2', link:'link2'},
+      {menu:'menu3', link:'link3'},
+      {menu:'menu4', link:'link4'},
+      {menu:'menu5', link:'link5'},
+      {menu:'menu6', link:'link6'},
+      {menu:'menu7', link:'link7'},
+      {menu:'menu8', link:'link8'},
+      {menu:'menu9', link:'link9'}
+    ]
   }
- menus=["Menu1","Menu2","Menu3","Menu4","Menu5","Menu6","Menu7","Menu8","Menu9"];
- links=["Link1","Link2","Link3","Link4","Link5","Link6","Link7","Link8","Link9"];
+ 
+
 }
